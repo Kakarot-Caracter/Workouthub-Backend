adb3c2071671d5b00ef3768d2894193dfb2ac3e7
Kakarot-Caracter <itachimartinez0@gmail.com>
2025-10-24 13:48:32 -0300

    Migrate to PostgreSQL, add Swagger, remove auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { RoutinesModule } from './routines/routines.module';
-import { ExercisesModule } from './exercises/exercises.module';
+import { PrismaModule } from './database/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { RoutinesModule } from './modules/routines/routines.module';
+import { ExercisesModule } from './modules/exercises/exercises.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, RoutinesModule, ExercisesModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    RoutinesModule,
+    ExercisesModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
