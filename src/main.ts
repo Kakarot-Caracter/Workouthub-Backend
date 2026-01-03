36d7d873a373f502f6bab1046f0009ed1517cef5
Kakarot-Caracter <itachimartinez0@gmail.com>
2026-01-03 03:35:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.useGlobalFilters(new PrismaClientExceptionFilter());
 
   await app.register(fastifyCors, {
-    origin: 'http://localhost:3000',
+    origin: 'https://workouthub-frontend.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
