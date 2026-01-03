662bbb58e7442544e93a9ecb457fcbc8c92ec08b
Kakarot-Caracter <itachimartinez0@gmail.com>
2026-01-03 02:49:53 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.useGlobalFilters(new PrismaClientExceptionFilter());
 
   await app.register(fastifyCors, {
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
